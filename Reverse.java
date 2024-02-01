cb14d8775c7c0912511900ecc874eecc4f44053d
david <david.evenari@post.runi.ac.il>
2024-02-01 21:17:15 +0200

    22

--- Reverse.java
+++ Reverse.java
@@ -3,6 +3,7 @@ public class Reverse {
     public static void main(String[] args) {
         String str = args[0];
         int n = (str.length())-1;
+		int length = (str.length());
        
         
         while (n != -1) {
@@ -10,8 +11,15 @@ public class Reverse {
             n-- ;
            
         }
+        int middlenum =(length/2);
+		char middle =str.charAt(middlenum);
         System.out.println(" ");
-        System.out.println("The middle character is " + str.charAt(n/2));
+      
+		
+	     System.out.println("The middle character is " + middle);
+	      
 
-    }
-}
+	 }
+ }
+	
+    
